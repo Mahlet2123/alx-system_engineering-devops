f8183315e3d3f6d4ba995ec6438584cbc12ff4a9
mahlet <akotet2123@gmail.com>
2023-03-26 15:41:09 -0700

    try puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,11 @@
-# Puppet to make changes to our configuration file
-
-file { 'config':
-  ensure  => file,
-  path    => '/root/.ssh/config',
-  content => '
-
-Host *
-    PasswordAuthentication no
-
-Host myserver
-    Hostname 54.90.3.149
-    User ubuntu
-    IdentityFile ~/.ssh/school
-'
+#connect to server without a password
+file_line { 'no password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+file_line { 'change private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school'
 }
