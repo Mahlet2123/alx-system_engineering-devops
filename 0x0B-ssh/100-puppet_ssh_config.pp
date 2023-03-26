d4fbcc09d86258bef5bec07deaae790f501af186
mahlet <akotet2123@gmail.com>
2023-03-26 15:02:46 -0700

    Puppet to make changes to our configuration file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# Puppet to make changes to our configuration file
+
+file { 'config':
+  ensure  => file,
+  path    => '~/.ssh/config'
+  content => '
+
+Host *
+    PasswordAuthentication no
+
+Host myserver
+    Hostname 54.90.3.149
+    User ubuntu
+    IdentityFile ~/.ssh/school'
+}
