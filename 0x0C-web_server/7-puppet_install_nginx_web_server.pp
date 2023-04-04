f5f0ba0fbefcce97438f271bfff790da84687487
mahlet <akotet2123@gmail.com>
2023-04-04 04:08:32 -0700

    configuring your server with Puppet!

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,5 +2,11 @@
 
 exec {'install':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/lexxyla permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ;',
 }
+
+exec {'/usr/bin/env echo "Hellow World" > /var/www/html/index.html': }
+exec {'/usr/bin/env sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.holbertonschool.com permanent;" /etc/nginx/sites-available/default': }
+exec {'/usr/bin/env sed -i "/server_name _;/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
+exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html': }
+exec {'/usr/bin/env service nginx start': }
