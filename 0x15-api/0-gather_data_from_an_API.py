09c2fe82b842c5c30ab315b5bf74cb33c7c861cf
mahlet <akotet2123@gmail.com>
2023-04-25 11:10:43 +0000

    bug fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     completed = []
     for todo in todo_response:
-        if todo.get("completed") is True:
+        if todo.get("completed"):
             completed.append(todo.get("title"))
     print(
         "Employee {} is done with tasks({}/{}):".format(
@@ -28,21 +28,3 @@ if __name__ == "__main__":
     )
     for complete in completed:
         print("\t {}".format(complete))
-
-    """
-    EMPLOYEE_NAME = name_response[0].get('name')
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
-    for i in todo_list:
-        if i.get("completed"):
-            NUMBER_OF_DONE_TASKS = NUMBER_OF_DONE_TASKS + 1
-        TOTAL_NUMBER_OF_TASKS = TOTAL_NUMBER_OF_TASKS + 1
-    print(
-        "Employee {} is done with tasks({}/{})".format(
-            EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS
-        )
-    )
-    for i in todo_list:
-        if i.get("completed"):
-            title = i.get("title")
-            print("\t {}".format(title))"""
