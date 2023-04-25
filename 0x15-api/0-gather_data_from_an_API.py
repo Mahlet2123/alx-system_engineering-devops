b27ee67f18a9c05b153995779755858928d3346f
mahlet <akotet2123@gmail.com>
2023-04-25 04:24:07 -0700

    correction

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,23 +8,27 @@ import sys
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    url_todo = url + "todos"
-    url_name = url + "users/{}".format(sys.argv[1])
+    url_todo = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        sys.argv[1])
+    url_name = "https://jsonplaceholder.typicode.com/users?id={}".format(
+        sys.argv[1])
 
-    params = {"userId": sys.argv[1]}
+    todo_list = requests.get(url_todo).json()
+    name_response = requests.get(url_name).json()
 
-    todo_response = requests.get(url=url_todo, params=params).json()
-    name_response = requests.get(url=url_name).json()
+    EMPLOYEE_NAME = name_response[0].get('name')
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    for i in todo_list:
+        if i.get("completed"):
+            NUMBER_OF_DONE_TASKS = NUMBER_OF_DONE_TASKS + 1
+        TOTAL_NUMBER_OF_TASKS = TOTAL_NUMBER_OF_TASKS + 1
 
-    completed = []
-    for todo in todo_response:
-        if todo.get("completed"):
-            completed.append(todo.get("title"))
     print(
         "Employee {} is done with tasks({}/{}):".format(
-            name_response.get("name"), len(completed), len(todo_response)
+            EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS
         )
     )
-    for complete in completed:
-        print("\t {}".format(complete))
+    for complete in todo_list:
+        if complete.get("completed"):
+            print("\t {}".format(complete.get("title")))
