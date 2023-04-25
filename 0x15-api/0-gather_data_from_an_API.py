a9dba5b55afdc28c4f680646cc8d09a2a85cd42c
mahlet <akotet2123@gmail.com>
2023-04-24 18:48:38 -0700

    check bug fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-"""
-for a given employee ID, returns information
-about his/her TODO list progress.
-"""
+"""Script that uses REST API"""
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    url_todo = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        sys.argv[1])
-    url_name = "https://jsonplaceholder.typicode.com/users?id={}".format(
-        sys.argv[1])
-    todo_response = requests.get(url_todo)
-    name_response = requests.get(url_name).json()
-    todo_list = todo_response.json()
-    EMPLOYEE_NAME = name_response[0].get('name').strip()
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
-    for i in todo_list:
-        if i.get("completed"):
-            NUMBER_OF_DONE_TASKS = NUMBER_OF_DONE_TASKS + 1
-        TOTAL_NUMBER_OF_TASKS = TOTAL_NUMBER_OF_TASKS + 1
-    print(
-        "Employee {} is done with tasks({}/{})".format(
-            EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS
-        )
-    )
-    for i in todo_list:
-        title = i.get("title")
-        print("\t{}".format(title))
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        args = {"id": sys.argv[1]}
+        users = requests.get("https://jsonplaceholder.typicode.com/users",
+                             params=args).json()
+        args = {"userId": sys.argv[1]}
+        todos = requests.get("https://jsonplaceholder.typicode.com/todos",
+                             params=args).json()
+        todos_len = 0
+        todos_arr = []
+        for i in todos:
+            if i.get("completed"):
+                todos_arr.append(i)
+                todos_len += 1
+
+        print("Employee {} is done with tasks({}/{}):".format(
+              users[0].get("name"), todos_len, len(todos)))
+
+        for i in todos_arr:
+            print("\t {}".format(i.get("title")))
