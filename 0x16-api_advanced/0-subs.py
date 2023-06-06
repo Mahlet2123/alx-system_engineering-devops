e2eb11fb80b7605fce0e62c74b7c6f196f647667
mahlet <akotet2123@gmail.com>
2023-06-06 11:19:14 +0000

    PEP fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,15 @@ total subscribers) for a given subreddit.
 If an invalid subreddit is given, the function should return 0.
 """
 
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "My API Client"}  # Custom User-Agent header
+    headers = {"User-Agent": "My API Client"}
+    # Custom User-Agent header
+    
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
