20ba3f2af28c4b3f8a5b88d758686ee1eaf60d1b
mahlet <akotet2123@gmail.com>
2023-06-06 11:19:50 +0000

    PEP fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "My API Client"}
     # Custom User-Agent header
-    
+
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
