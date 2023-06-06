fd8e9ba750411af84294c6534b1b17467887006b
mahlet <akotet2123@gmail.com>
2023-06-06 14:50:17 +0000

    print top 10 titles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Write a function that queries the Reddit API
-and prints the titles of the first 10 hot posts 
+and prints the titles of the first 10 hot posts
 listed for a given subreddit.
 
 If not a valid subreddit, print None.
@@ -9,16 +9,25 @@ If not a valid subreddit, print None.
 
 
 import requests
+import json
 
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "My API Client"}
+    url = "https://www.reddit.com/r/{}/top.json".format(subreddit)
+    headers = {"User-Agent": "Linux:0-subs:v1.0"}
     # Custom User-Agent header
 
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        return data["data"]["subscribers"]
+        #print(json.dumps(data, indent=4))
+        top_list = data["data"]["children"]
+        count = 0
+        for i in top_list:
+            if count >= 10:
+                break
+            print(i["data"]["title"])
+            count = count + 1
+
     else:
-        return 0
+        print("None")
